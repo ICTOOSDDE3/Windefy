10cd58573c324e36c480b082831c5c5dccca62ec
dylanroubos <dylanroubos@gmail.com>
2020-11-16 17:47:46 +0100

    Added email system + base layout for registration system

---
+++ Controller/Controller_Mail.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Linq;
+using Model;
+
+namespace Controller
+{
+    class Controller_Mail
+    {
+        private static Random random = new Random();
+        private MailMessage email;
+
+        //TODO: Refactor
+        public void SendMail(Model_Mail Mail)
+        {
+            //Instanciate email
+            email = new MailMessage();
+            //Set sender
+            email.From = new MailAddress(Mail.SenderEmail);
+            //Set receiver
+            email.To.Add(new MailAddress(Mail.RecipientEmail));
+            //Set subjec
+            email.Subject = Mail.Subject;
+            //Set email messagew
+            email.Body = Mail.Message;
+            //Set priority
+            email.Priority = MailPriority.High;
+
+            SmtpClient emailSender = new SmtpClient("mail.software-essentials.nl");
+
+            // Set mail credentials
+            emailSender.Credentials = new NetworkCredential("username", "password");
+            // Send the email
+            emailSender.Send(email);
+        }
+
+        public void SendValidationMail(string adres)
+        {
+            string code = CreateCode();
+
+            Model_Mail mail = new Model_Mail(adres, "dylanroubos@software-essentials.nl", "Mail verification", $"To verify your mail, insert code: {code}, \n\n kind regards, \n\n Windify ");
+            SendMail(mail);
+        }
+
+        public string CreateCode()
+        {
+            //Linq statement to create random string based on the given chars and amount
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 10)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
+
+
---
+++ Controller/Controller_Register.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Controller
+{
+    class Controller_Register
+    {
+        //Method to register an account (validate and save to db)
+        public void RegisterAccount(string email, string name, string pw1, string pw2)
+        {
+            if (IsValidEmail(email))
+            {
+                if (IsEmailUnique(email))
+                {
+                    if (ArePasswordsEqual(pw1, pw2))
+                    {
+                        //TODO: Create the account in the database when the db connection is made
+
+                        //Hash the password
+
+                        //INSERT INTO Users (User_Email, User_Name, User_Password)
+                        //VALUES (email, name, pw1)
+                    }
+                }
+            }
+        }
+
+        //Check if email is valid
+        public bool IsValidEmail(string email)
+        {
+            return new EmailAddressAttribute().IsValid(email);
+        }
+
+        //Check if two passwords are equal
+        public bool ArePasswordsEqual(string str1, string str2)
+        {
+            return str1.Equals(str2);
+        }
+
+        //Check if email is already in use
+        public bool IsEmailUnique(string email)
+        {
+            //TODO: Add code to check if email already excists in database when db connection is written
+            return true;
+        }
+    }
+}
+
