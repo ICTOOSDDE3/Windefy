b18d330a902406000433262fbcc463a80ae88397
s1141585 <s1141585@student.windesheim.nl>
2020-11-16 10:32:02 +0100

    Initial commit

---
+++ Model/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Model
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
