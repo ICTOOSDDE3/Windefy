bb17baf957f0e25bd79b587c4477aa7922222848
Berkay Erdal <S1144113@student.windesheim.nl>
2020-11-17 14:48:17 +0100

    Verbinding met database. Werkt nog niet.

--- Model/Program.cs
+++ Model/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Renci.SshNet;
+using System;
 
 namespace Model
 {
@@ -6,7 +8,15 @@ namespace Model
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PasswordConnectionInfo connectionInfo = new PasswordConnectionInfo("145.44.235.109", "student", "S3q8yCze6NL");
+            connectionInfo.Timeout = TimeSpan.FromSeconds(30);
+            var client = new SshClient(connectionInfo);
+            client.Connect();
+            ForwardedPortLocal portFwld = new ForwardedPortLocal("127.0.0.1", Convert.ToUInt32(1433), "127.0.0.1", Convert.ToUInt32(1433)); client.AddForwardedPort(portFwld);
+            portFwld.Start();
+            // using Renci.sshNet 
+            var connection = new MySqlConnection("server = " + "127.0.0.1" + "; Database = TestDB; password = S3q8yCze6NL; UID = SA; Port = 1433");
+            connection.Open();
         }
     }
 }
