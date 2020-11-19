71a1cc605296c1f499b92c49f44ba636eb925f06
s1141585 <s1141585@student.windesheim.nl>
2020-11-19 14:09:45 +0100

    registration and a part of WPF for login

--- Controller/Controller_Register.cs
+++ Controller/Controller_Register.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 namespace Controller
 {
@@ -18,10 +19,13 @@ namespace Controller
                     {
                         //TODO: Create the account in the database when the db connection is made
 
-                        //Hash the password
+                        byte[] bytePassword = PasswordToByte(pw1);
+                        var salt = GenerateSalt(20); // maybe a random number between 20 - 30?
+                        byte[] genratedPasswordHash = GenerateHash(bytePassword, salt, 10, 10); // maybe these numbers random generate aswell?
 
-                        //INSERT INTO Users (User_Email, User_Name, User_Password)
-                        //VALUES (email, name, pw1)
+                        //INSERT INTO Users (User_Email, User_Name, User_Password(GenratedPasswordHash)) 
+                        //we also need to store the salt (generated above) and the iterations and workfactor (which are the two 10's in GenerateHash())
+                        //VALUES (email, name, generatedPasswordHash)
                     }
                 }
             }
@@ -46,11 +50,50 @@ namespace Controller
             return true;
         }
 
-        public string PasswordToHash(string password)
+        /// <summary>
+        ///generate a byte array from a password
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>bytePassword</returns>
+        public byte[] PasswordToByte(string password)
         {
-            string hash = "";
-            return hash;
+            byte[] bytePassword = Encoding.ASCII.GetBytes(password);
+            return bytePassword;
         }
+
+        /// <summary>
+        /// generate a salt byte array
+        /// </summary>
+        /// <param name="length">amount of bytes</param>
+        /// <returns>byte array with the given length</returns>
+        public byte[] GenerateSalt(int length)
+        {
+            var bytes = new byte[length];
+
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes);
+            }
+
+            return bytes;
+        }
+
+        /// <summary>
+        /// Generates a hash with the bytepassword and the byte salt combined
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="salt"></param>
+        /// <param name="iterations"></param>
+        /// <param name="length"></param>
+        /// <returns>a byte array of the bytepassword combined with the byte salt</returns>
+        public byte[] GenerateHash(byte[] password, byte[] salt, int iterations, int length)
+        {
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, iterations))
+            {
+                return deriveBytes.GetBytes(length);
+            }
+        }
+
     }
 }
 
