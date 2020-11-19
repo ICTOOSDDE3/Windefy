d1aa5bf5df370c1866103a874bc865f4d62681da
Thomas <thomasvandeweg@hotmail.nl>
2020-11-19 09:33:34 +0100

    git fix

--- View/AudioVisueel.xaml.cs
+++ View/AudioVisueel.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using Microsoft.Win32;
+using System.Windows.Controls.Primitives;
 
 namespace View
 {
@@ -19,19 +20,23 @@ namespace View
 	/// </summary>
 	public partial class AudioVisueel : Window
 	{
-		private MediaPlayer mediaPlayer = new MediaPlayer();
+		public MediaPlayer mediaPlayer = new MediaPlayer();
+		private bool IsDragging = false;
 
 		public AudioVisueel()
 		{
 			InitializeComponent();
-			OpenFileDialog openFileDialog = new OpenFileDialog();
-			openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
-			if (openFileDialog.ShowDialog() == true)
+			/* OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
 			{
 				mediaPlayer.Open(new Uri(openFileDialog.FileName));
 			}
+			*/
+			mediaPlayer.Open(new Uri("https://www.learningcontainer.com/wp-content/uploads/2020/02/Kalimba.mp3"));
 			Song.Content = mediaPlayer.Source;
-			DispatcherTimer timer = new DispatcherTimer();
+            mediaPlayer.MediaEnded += MusicControl
+            DispatcherTimer timer = new DispatcherTimer();
 			timer.Interval = TimeSpan.FromSeconds(1);
 			timer.Tick += timer_Tick;
 			timer.Start();
@@ -39,29 +44,30 @@ namespace View
 
 		void timer_Tick(object sender, EventArgs e)
 		{
-			if (mediaPlayer.Source != null)
+			if (mediaPlayer.Source != null && !IsDragging)
 			{
 				lblStatus.Content = String.Format("{0} / {1}", mediaPlayer.Position.ToString(@"hh\:mm\:ss"), mediaPlayer.NaturalDuration.TimeSpan.ToString(@"hh\:mm\:ss"));
 				pbStatus.Value = (mediaPlayer.Position.TotalSeconds / mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds) * 100;
+				sliderStatus.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+				sliderStatus.Value = mediaPlayer.Position.TotalSeconds;
 			}
 			else
 				lblStatus.Content = "No file selected...";
 		}
+		public void InsertSong() //Parameter Song class
+        {
 
-		private void btnPlay_Click(object sender, RoutedEventArgs e)
-		{
+        }
+		private void TogglePlay(object sender,RoutedEventArgs e)
+        {
 			mediaPlayer.Play();
+			tbPlayPause.Content = "Pause";
 		}
-
-		private void btnPause_Click(object sender, RoutedEventArgs e)
-		{
+		private void TogglePause(object sender, RoutedEventArgs e)
+        {
 			mediaPlayer.Pause();
-		}
-
-		private void btnStop_Click(object sender, RoutedEventArgs e)
-		{
-			mediaPlayer.Stop();
-		}
+			tbPlayPause.Content = "Play";
+        }
 
 		private void btnNext_Click(object sender, RoutedEventArgs e)
 		{
@@ -83,5 +89,21 @@ namespace View
 			//moet nog
 		}
 
-	}
+        private void sliderStatus_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void sliderStatus_DragLeave(object sender, DragEventArgs e)
+        {
+			IsDragging = false;
+			mediaPlayer.Position = TimeSpan.FromSeconds(sliderStatus.Value);
+        }
+
+        private void sliderStatus_DragEnter(object sender, DragEventArgs e)
+        {
+			IsDragging = true;
+        }
+		
+    }
 }
