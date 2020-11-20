b77cff908f11ae08740cef8167bfbfd2b0e57248
Dylan Roubos <s1141496@student.windesheim.nl>
2020-11-20 14:27:39 +0100

    Extended user class and made static

--- View/obj/Debug/netcoreapp3.1/App.g.cs
+++ View/obj/Debug/netcoreapp3.1/App.g.cs
@@ -47,7 +47,7 @@ namespace View {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace View {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "4.8.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.0.0")]
         public static void Main() {
             View.App app = new View.App();
             app.InitializeComponent();
