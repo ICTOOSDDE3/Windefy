a99755f468ae0a81091c2ba87612df43287b3f1c
Dylan Roubos <s1141496@student.windesheim.nl>
2020-11-20 09:45:24 +0100

    Added password controller with a method to get the password from the JSON File. Added JSON File to keep track of the passwords. Added some Nunit tests to check if controller functions as expected

---
+++ Controller/Passwords.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace Controller
+{
+    public static class Passwords
+    {
+        //Get a specifick password from the Password JSON FIle
+        public static string GetPassword(string type)
+        {
+            //Create the JSON object in the class from the JSON File
+            JObject data = JObject.Parse(File.ReadAllText("..\\..\\..\\..\\Model/Passwords.JSON"));
+            //Get the matched data
+            var returnValue = data["Credentials"]["Passwords"][type];
+
+            //Check if data is not null
+            if (returnValue != null)
+            {
+                //Return the data
+                return returnValue.ToString();
+            }
+            else
+            {
+                //Return null if there is no password found
+                return null;
+            }
+        }
+    }
+}
--- Controller/Program.cs
+++ Controller/Program.cs
@@ -8,13 +8,6 @@ namespace Controller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            JObject data = JObject.Parse(File.ReadAllText("..\\..\\..\\..\\TEST.JSON"));
-
-            var joe = data["Credentials"]["Passwords"]["DB"];
-
-            Console.WriteLine(joe);
         }
     }
 }
