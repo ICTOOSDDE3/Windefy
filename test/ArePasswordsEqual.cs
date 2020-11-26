55772bc2f91d072399da9e23a642f3a7a58bdb5a
s1141585 <s1141585@student.windesheim.nl>
2020-11-26 16:26:46 +0100

    Registration finished, verification finished, login finished after registration or login in there will an user be made

---
+++ test/ArePasswordsEqual.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    [TestFixture]
+    class ArePasswordsEqual
+    {
+        Controller.Register register = new Controller.Register();
+        [Test]
+        // test if passwords are equal should return false
+        public void ArePasswordsEqual_SimilarPasswords_ReturnsFalse()
+        {
+            string password1 = "password";
+            string password2 = "password2";
+
+            var isNotEqual = register.ArePasswordsEqual(password1, password2);
+            Assert.AreEqual(isNotEqual, false);
+        }
+
+        [Test]
+        // test if passwords are equal should return true
+        public void ArePasswordsEqual_SimilarPasswords_ReturnsTrue()
+        {
+            string password1 = "password";
+            string password2 = "password";
+
+            var isNotEqual = register.ArePasswordsEqual(password1, password2);
+            Assert.AreEqual(isNotEqual, true);
+        }
+    }
+}
