0c95f4bfa3c32ab3c6a476716a1cd21677b04244
Dylan Roubos <s1141496@student.windesheim.nl>
2020-12-15 13:55:58 +0100

    Added artist to the playlist view

--- Model/Track.cs
+++ Model/Track.cs
@@ -10,7 +10,8 @@ namespace Model
         public string Title { get; set; }
         public int Listens { get; set; }
         public int LanguageID { get; set; }
-        public int Duration { get; set; }
+        public double Duration { get; set; }
+        public string DurationView { get; set; }
         public DateTime Date_Created { get; set; }
         public int NumberID { get; set; }
         public List<Artist> Artists { get; set; }
@@ -18,10 +19,14 @@ namespace Model
         public string File_path { get; set; }
         public string Image_path { get; set; }
 
-        public Track(int trackID, string title)
+        public Track(int trackID, string title, int durationInSec)
         {
             TrackID = trackID;
             Title = title;
+            Duration = durationInSec / 60;
+            var tempDur = Math.Round((double)durationInSec / 60, 2, MidpointRounding.AwayFromZero);
+            DurationView = string.Format("{0:00.00}", tempDur).Replace(".", ":");
+            Artists = new List<Artist>();
         }
 
         public Track()
