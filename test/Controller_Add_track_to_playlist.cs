6d7e225394b71652ee5e9af9a76340a591d88470
Jort Kuhlmann <jort@loylap.com>
2020-12-15 10:34:30 +0100

    test

---
+++ test/Controller_Add_track_to_playlist.cs
@@ -0,0 +1,58 @@
+﻿using Controller;
+using NUnit.Framework;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    [TestFixture]
+    class Controller_Add_track_to_playlist
+    {
+        AddMusicToPlaylist controller = new AddMusicToPlaylist();
+        Playlist controllerPlaylist = new Playlist();
+        int playlistId = 0;
+
+        [SetUp]
+        public void Init()
+        {
+            DBConnection.Initialize();
+            Playlist playlist = new Playlist();
+            playlistId = playlist.CreateUserPlaylist("TESTaaa", false);
+        }
+
+        [Test]
+        public void add_tracks_to_playplist()
+        {
+            controller.InsertToPlaylist(playlistId, 10);
+            controller.InsertToPlaylist(playlistId, 140);
+
+            DBConnection.OpenConnection();
+
+            string query = $"SELECT count(*) FROM playlist_track WHERE playlistID = {playlistId}";
+
+            SqlCommand cmd = new SqlCommand(query, DBConnection.Connection);
+
+            int total = (int)cmd.ExecuteScalar();
+
+            DBConnection.CloseConnection();
+
+            Assert.AreEqual(total, 2);
+        }
+
+        [TearDown]
+        public void delete()
+        {
+            DBConnection.OpenConnection();
+
+            string query = $"DELETE FROM playlist_track WHERE playlistID = {playlistId}";
+            string query2 = $"DELETE FROM playlist WHERE playlistID = {playlistId}";
+
+            SqlCommand cmd = new SqlCommand(query, DBConnection.Connection);
+            cmd.ExecuteNonQuery();
+
+            SqlCommand cmd2 = new SqlCommand(query2, DBConnection.Connection);
+            cmd2.ExecuteNonQuery();
+
+            DBConnection.CloseConnection();
+        }
+    }
+}
